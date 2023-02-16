6a14977fb9b1f5c16c545f5335de22e71200fb9a
Wilson <wilyugi26@gmail.com>
2023-02-16 14:28:36 -0800

    Task 5

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with that printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("But the outcome is a piece of art");
+return (0);
+}
