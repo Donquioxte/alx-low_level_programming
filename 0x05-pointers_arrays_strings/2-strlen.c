9892271e145e5be2008b4a259336a863e0c976ae
Wilson <wilyugi26@gmail.com>
2023-02-28 16:35:13 -0800

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,16 @@
 /**
  * _strlen - a function that returns the length of a string.
  *
- * *s: A character to be measured
+ * @s: A character to be measured
  *
- * Return length of string
+ * Return: length of string
  */
 
 int _strlen(char *s)
 {
 	size_t length = 0;
-	while (*s ++)
+
+	while (*s++)
 		length++;
 	return (length);
 }
