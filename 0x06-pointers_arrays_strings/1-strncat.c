b7738da76feb6ad3deb1c37cb2d509cf6f628024
Wilson <wilyugi26@gmail.com>
2023-03-02 15:49:40 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,12 +3,11 @@
 /**
  * _strncat - a function that concatenates two strings using
  * at most n bytes
- *
  * @dest: A string in the function
  * @src: Another string in the function
  * @n: An integer in the function
  *
- * Return dest(Success)
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
