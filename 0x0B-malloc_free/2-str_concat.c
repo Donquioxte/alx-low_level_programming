b648a802ef8ada7e46fbe9ff5ea24045f8cfd21a
Wilson <wilyugi26@gmail.com>
2023-03-14 16:01:04 -0700

    Task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 
 		s1 = "";
-	
+
 	if (s2 == NULL)
 
 		s2 = "";
 
-		
+
 	i = ci = 0;
 
 	while (s1[i] != '\0')
@@ -46,8 +46,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
 }
